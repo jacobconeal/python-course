def854e14ec31418014e81a0d5cc0b07ab91b53a
jacobconeal <jacobconeal@students.abtech.edu>
2026-01-25 16:17:58 -0500

    Add files via upload

---
+++ number_8.py
@@ -0,0 +1,11 @@
+# 2-8. Number 8
+# This program performs addition, subtraction, multiplication, and division
+# Each operation results in the number 8
+# addition
+print(5 + 3)  # 5 plus 3 equals 8
+# subtraction
+print(12 - 4)  # 12 minus 4 equals 8
+# multiplication
+print(2 * 4)  # 2 times 4 equals 8
+# division
+print(16 / 2)  # 16 divided by 2 equals 8
