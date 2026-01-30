be137c05ec2c50160483ce38991083c3930a88b4
jacobconeal <jacobconeal@students.abtech.edu>
2026-01-30 13:19:41 -0500

    Add files via upload

---
+++ my_cars.py
@@ -0,0 +1,7 @@
+# 3-3. My Own List
+# This program stores my favorite cars and prints statements about them
+
+favorite_cars = ["Chevrolet Camaro", "Ford Mustang", "Dodge Challenger", "Tesla Model 3"]
+
+for car in favorite_cars:
+    print(f"I would like to own a {car}.")
