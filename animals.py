9935ecb2bf79ff74ea5b1645321fd9567f85d555
jacobconeal <jacobconeal@students.abtech.edu>
2026-02-11 18:51:36 -0500

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,13 @@
+# 4-2. Animals
+
+animals = ["polar bear", "dog", "tiger"]
+
+for animal in animals:
+    print(animal)
+
+print()
+
+for animal in animals:
+    print(f"A {animal} is a powerful mammal.")
+
+print("\nAll of these animals are mammals!")
