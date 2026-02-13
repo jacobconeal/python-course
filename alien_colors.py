15da21f369d1d68b8e16f5f6356043612de720f7
jacobconeal <jacobconeal@students.abtech.edu>
2026-02-12 19:27:18 -0500

    Add files via upload

---
+++ alien_colors.py
@@ -0,0 +1,11 @@
+# 5-5. Alien Colors
+# This program awards points based on the alien's color.
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+elif alien_color == "yellow":
+    print("You earned 10 points!")
+else:
+    print("You earned 15 points!")
